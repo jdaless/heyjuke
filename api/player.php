0f71dd8ec6dc48f9d9ed20e07f495aad151024df
John D'Alessandro <dalessandro.john.24+github@gmail.com>
2016-08-28 17:04:39 -0400

    networking functional

--- api/player.php
+++ api/player.php
@@ -1,3 +1,13 @@
 <?php
-	
+$url = 'http://localhost:15000';
+$data = 'ChemicalWarfare.mp3';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+curl_close($ch);
+if ($result === FALSE) { /* Handle error */ }
+
+echo 'Result: ';
+var_dump($result);
 ?>
--- player.py
+++ player.py
@@ -1,21 +1,66 @@
 import os
 import json
+import socket
+import threading
 import pygame
 
 dirName = os.path.dirname(os.path.abspath(__file__))
 
-queueFile = open('queue.json', 'r+')
+queue = []
+currentlyPlaying = ''
 
 pygame.mixer.init()
 
-while(True):
-	queue = json.load(queueFile)
-	play = queue.pop()
-	queueFile.seek(0)
-	queueFile.truncate()
-	json.dump(queue, queueFile)
-	pygame.mixer.music.load('music\\' + play['path'])
-	pygame.mixer.music.play()
-	while(pygame.mixer.music.get_busy()):
-		continue
 
+def playQueue():
+	while(True):
+		if(len(queue) > 0):
+			play = queue.pop()
+			currentlyPlaying = play
+			pygame.mixer.music.load('music\\' + play)
+			pygame.mixer.music.play()
+			while(pygame.mixer.music.get_busy()):
+				continue
+		else:
+			currentlyPlaying = ''
+
+def socketListener():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(("localhost",15000))
+	s.listen(1)
+	while(True):
+		print("Waiting for a connection...")
+		connection, sender = s.accept()
+		print(sender)
+		try:
+			data = connection.recv(128)
+			print('received "%s"', data)
+			if data:
+				print('sending data back to the client')
+				connection.sendall(data)
+				print('data sent')
+            
+		finally:
+			# Clean up the connection
+			connection.close()
+
+socketThread = threading.Thread(target = socketListener)
+
+playerThread = threading.Thread(target = playQueue)
+
+socketThread.daemon = True
+playerThread.daemon = True
+
+playerThread.start()
+socketThread.start()
+
+while(True):
+	x = input()
+	if(x=="break"):
+		break 
+	if(x=="queue"):
+		print(queue)
+	if(x=="playing"):
+		print(currentlyPlaying)
+	if(x.startswith("add")):
+		queue.append(x[4:])
